4e87c539d9b051c8704377dc72d88459157eee01
Chris-matrix <cwatt0046@launchapd.org>
2024-09-26 17:16:35 -0400

    youtube coode

--- calclatorEarnings.js
+++ calclatorEarnings.js
@@ -1,1 +1,19 @@
+/ Global variables 
 
+document.addEventListener("DOMContentLoaded", () =>{
+  
+    // get the HTML button 
+    const btn = document.querySelector("calcu-earnings-btn");
+    console.log(" get caluate bnt ", btn);
+
+
+   // wire up the event listner 
+    btn.addEventListener("click", (evt) => {
+
+        console.log("Clicked "); 
+        
+        evt.defaultPrevented();
+        const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+        document.body.style.backgroundColor = rndCol;
+      });
+})
---
+++ chatapp.js
@@ -0,0 +1,5 @@
+function funky(){
+    console.log()
+    \
+
+}
